a936814d29cf848b3b360bf9adb7a91787f34e33
Joost van de Weijer <vdweijer@ling.lu.se>
2019-04-14 09:28:12 +0200

    added kitten columns to the datafile

--- R/constants.R
+++ R/constants.R
@@ -1,9 +1,9 @@
 cats=structure(paste("c",sprintf("%02.0f",1:70),sep=""),
    names=c("Donna","Rocky","Turbo","Vimsan","Kompis","Sixten","Sotarn","Zoran","Moses",
-      "Tass","Piper","Munnen","Monk","Runten","Moriarty","Mycroft","Sherlock","Irene",
-      "Mrs Hudson","Watson","Simba","Troll","Malte","Shiva","Indigo","Abbey Road","Doris",
+      "Tass","Piper","Monk","Runten","Sherlock","Watson","Mycroft","Moriarty","Mrs Hudson",
+      "Irene","Munnen","Simba","Troll","Malte","Shiva","Indigo","Abbey Road","Doris",
       "Maja","Samira","Göran","Kismet","Dolly","Ani","Neko","Zita","Flisan","Pusjkin",
-      "Dator","Nino","Tamar","Zenya","Toulouse","Elliot","Giza","Vincent","Lizzy","Leo",
+      "Dato","Nino","Tamar","Zenya","Toulouse","Elliot","Giza","Vincent","Lizzy","Leo",
       "Rufus","Mauro","Anita","Astrid","Yoshi","Sushi","Sotis","Persika","Shanti","Sasha",
       "Sotis","Tiger","Tintin","Gunilla","Marianne","Klös","Fräs","Zäta","Zombie",
       "Ragnar","Zebra","Tassitus","Väsen"))
@@ -12,4 +12,5 @@ f0steps=paste("f0",1:100,sep="_")
 
 trafficlights=structure(c("green3","orange","orangered2"),names=c("green","orange","red")) #
 
-kittens=structure(paste("c",12:20,sep=""),names=c("Monk","Runten","Sherlock","Watson","Mycroft","Moriarty","Mrs Hudson","Irene","Munnen"))
+kittens=structure(c(paste("c",14:19,sep=""),paste("c",38:40,sep="")),
+   names=c("Sherlock","Watson","Mycroft","Moriarty","Mrs Hudson","Irene","Dato","Nino","Tamar"))
