9863f80163ef90aa0410eba3b0ccd75de5b4cb13
Joost van de Weijer <vdweijer@ling.lu.se>
2020-12-21 09:42:36 +0100

    new wide datafile

--- R/constants.R
+++ R/constants.R
@@ -9,8 +9,18 @@ cats=structure(paste("c",sprintf("%02.0f",1:70),sep=""),
       "Ragnar","Zebra","Tassitus","Väsen"))
 
 f0steps=paste("f0",1:100,sep="_")
+f0sem=paste(f0steps,"sem",sep=".")
+f0log=paste(f0steps,"log",sep=".")
+f0z=paste(f0steps,"z",sep=".")
 
-trafficlights=structure(c("green3","orange","orangered2"),names=c("green","orange","red")) #
+trafficlight=structure(c("green3","orange","orangered2"),names=c("green","orange","red")) #
 
 kittens=structure(c(paste("c",14:19,sep=""),paste("c",38:40,sep="")),
    names=c("Sherlock","Watson","Mycroft","Moriarty","Mrs Hudson","Irene","Dato","Nino","Tamar"))
+
+vocalisationtypes=structure(c("purr","meow","trill","chirp","growl","howl","snarl","hiss","spit",
+   "murmur","merged trill and meow","other","trill-meow","chirr-meow","meow-trill","growl-howl",
+   "growl-trill","trill-growl","growl-snarl","howl-growl","howl-lateral","trill-howl",
+   "meow-howl","howl-meow","trill-purr","purr-trill","chirp-trill","meow-purr","purr-meow"),names=
+   c("Pr","Me","Tr","Ch","Gr","Ho","Sn","Hs","Sp","Mm","Mt","Ot","TM","RM","MT","GH","GT",
+      "TG","GS","HG","HL","TH","MH","HM","TP","PT","CT","MP","PM"))
